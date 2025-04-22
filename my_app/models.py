80a87b637682a470daf72464e812458ca080f3b2
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-04-22 23:02:40 +0500

    update models.py

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class University(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+    university = models.ForeignKey(University, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
