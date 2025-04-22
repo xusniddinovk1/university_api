329db7bea941cbf9ca4120b835f02d7660c8192c
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-04-22 23:08:43 +0500

    update models.py

--- my_app/models.py
+++ my_app/models.py
@@ -11,7 +11,23 @@ class University(models.Model):
 
 class Faculty(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False)
-    university = models.ForeignKey(University, null=True, on_delete=models.SET_NULL)
+    university = models.ManyToManyField(University)
+
+    def __str__(self):
+        return self.name
+
+
+class Chair(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+    faculty = models.ManyToManyField(Faculty)
+
+    def __str__(self):
+        return self.name
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+    chair = models.ForeignKey(Chair, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
