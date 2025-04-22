102c771e80f3b5782bb10e8ae2342232354d8434
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-04-22 23:29:54 +0500

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_EY', '')
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 DEBUG = True
 
