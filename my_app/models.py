039cbda31500c096fb08a34ae2c52f7061c2999e
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-04-22 23:11:02 +0500

    update models.py

--- my_app/models.py
+++ my_app/models.py
@@ -31,3 +31,13 @@ class Group(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
+    group = models.ForeignKey(Group, null=True, on_delete=models.SET_NULL)
+    phone_number = models.CharField(max_length=10, null=False, blank=False, unique=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
